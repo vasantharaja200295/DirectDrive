6dc1b4e6a6fc1c7cc73eb2ee5ca07bcb431fecc9
vasantharaja200295 <vasantharaja200295@gmail.com>
2023-11-01 20:12:22 +0530

    compeleted file sharing

--- GoogleDriveService.py
+++ GoogleDriveService.py
@@ -1,6 +1,5 @@
 from Google import Create_Service
 
-
 class DriveService:
     def __init__(self) -> None:
         CLIENT_SECRET_FILE = "credentials.json"
@@ -9,30 +8,46 @@ class DriveService:
         SCOPES = ['https://www.googleapis.com/auth/drive']
 
         self.service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
+        folder_id='1f_OmKk4pBeFPzCqvyzWmlL_-m1laDMep'
+        self.query = f"parents = '{folder_id}'"
+        response = self.service.files().list(q=self.query).execute()
+        self.files = response.get('files', [])
 
-    def listFiles(self, folder_id='15_1P_z-Lu8z88Ccd6Af2uWv2MyRoB7No'):
-        query = f"parents = '{folder_id}'"
-        response = self.service.files().list(q=query).execute()
-        return response['files']
+    def refresh(self):
+        response = self.service.files().list(q=self.query).execute()
+        self.files = response.get('files', [])
+
+
+    def get_file_by_name(self, filename=""):
+        index = {item.get('name'): item for item in self.files}
+        return index.get(filename)
     
-    def getFile(self, file_id='1AiBMkeDEeIaRMcOGw8sp_gvMe7rtgb_6'):
+
+    def display(self, file_id=''):
         request_body = {
-            'role':'reader',
-            'type':'anyone'
+            'role': 'reader',
+            'type': 'anyone'
         }
+        baseUrl = "https://drive.google.com/uc?export=download&id="
 
-        baseUrl = f"https://drive.google.com/uc?export=download&id="
+        displayData = []
 
-        self.service.permissions().create(
-            fileId=file_id,
-            body=request_body
-        ).execute()
+        for item in self.files:
+            temp = {}
+            id = item.get('id')
+            temp['id'] = id
+            self.service.permissions().create(fileId=id, body=request_body).execute()
+            temp['image'] = baseUrl + id
+            temp['name'] = item.get('name')
+            displayData.append(temp)
 
-        share_link = baseUrl+file_id
 
-        return share_link
-    
+        return displayData
 
-    def testfile(self ,file_id='1AiBMkeDEeIaRMcOGw8sp_gvMe7rtgb_6' ):
-        baseUrl = f"https://drive.google.com/uc?export=download&id={file_id}"
-        return baseUrl
+    def download_file_by_id(self, file_id):
+        try:
+            request = self.service.files().get_media(fileId=file_id)
+            response = request.execute()
+            return response
+        except Exception as e:
+            return None
--- main.py
+++ main.py
@@ -1,88 +1,41 @@
-from flask import Flask, render_template, redirect, request, jsonify
+from flask import Flask, render_template, redirect, Response
 from dotenv import load_dotenv
 from flask_cors import CORS
+from pymongo import MongoClient
 from GoogleDriveService import DriveService
+import os
+
 
 load_dotenv('.env')
 
 app = Flask(__name__)
 cors = CORS(app)
 
-
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
-
-uri = "mongodb+srv://vasantharaja200295:cjcCN2FklWjw427W@cluster0.vpzkuqi.mongodb.net/?retryWrites=true&w=majority"
-client = MongoClient(uri, server_api=ServerApi('1'))
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
-
-
-db = client.mydb
-
 drive = DriveService()
-
-
 
 @app.route("/")
 def main():
-    return render_template("index.html")
 
-@app.route('/api/login', methods=['POST'])
-def login():
-    response_data=""
-    data = request.json['body']
-    print(data)
-    print("From the React app:",request.json['headers'])
-    headers = request.json['headers']
-    auth = headers['X-API-Key']
-    authrow = db.Apps.find_one({'appid':auth})
-    if authrow:
+    sampleData = drive.display()
 
-        row = db.users.find_one({'username':data['username']})
-        if row and row['password'] == data['password']:
-            response_data = {'message': 'login Successfull', "code":'ok'}
-            return jsonify(response_data)
-        else:
-            response_data = {'message': 'login failed', "code":'falied'}
-    return jsonify(response_data)
+    return render_template("index.html", data=sampleData)
 
-
-@app.route('/api/register', methods=['POST'])
-def register():
-    data = request.json  # Assuming you are sending JSON data
-    row = db.users.find_one({'username':data['username']})
-    if not row:
-        db.insert_one({'username':data['username'], "password":data["password"]})
-        response_data = {'message':'register sucessfull', "code":'ok'}
+@app.route('/file/<filename>')
+def get_file_by_name(filename):
+    data = drive.get_file_by_name(filename)
+    file_data = drive.download_file_by_id(file_id=data.get('id'))
+    
+    if file_data:
+        response = Response(file_data, content_type='image/jpeg')  # Set the content type according to your file type
+        response.headers['Content-Disposition'] = 'inline'
+        return response
     else:
-        response_data= {'message':'user already exists', "code":'exists'}
-    return jsonify(response_data)
-
-
-@app.route("/api/<appid>/hello")
-def test(appid):
-    row = db.Apps.find_one({"appid":appid})
-    if row :
-        print(row)
-        return jsonify({"message":"you are authorised", 'appName':row['appName']})
-
-    return jsonify({"message":"you are not authorised"}) 
-
-
-@app.route('/api/drive')
-def files():
-    data = drive.listFiles()
-    return jsonify(data)
-
-@app.route('/api/drive/<id>')
-def getFile(id):
-    data = drive.getFile(file_id=id)
-    return jsonify(data)
-
+        return "File not found", 404
+    
+@app.route('/refresh')
+def refresh():
+    drive.refresh()
+    return redirect('/')
 
 if __name__ == "__main__":
     app.run(debug=True)
