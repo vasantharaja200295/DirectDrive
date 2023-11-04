bbff54a24c99f5cd07b961ae7d995db7f8ae65be
vasantharaja200295 <vasantharaja200295@gmail.com>
2023-11-04 20:40:29 +0530

    Fixed the authentication issue

--- GoogleDriveService.py
+++ GoogleDriveService.py
@@ -14,6 +14,7 @@ class DriveService:
 
     def load(self):
         response = self.service.files().list(q=self.query).execute()
+        print(response)
         self.files = response.get('files', [])
 
 
